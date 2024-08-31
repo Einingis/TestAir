5c7ec65599a28e3faf7f7378f452b63c501e854e
Einingis <mintautas.e@gmail.com>
2024-08-31 11:29:59 +0300

    main page and fetch data

---
+++ components/Weather.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from "react-native";
+
+export default function Weather(weatherData: any) {
+
+
+  return (
+    <View>
+      <Text>New window</Text>
+    </View>
+  )
+
+}
--- styles.tsx
+++ styles.tsx
@@ -1,4 +1,17 @@
-import { StyleSheet, Text, View, Image } from 'react-native';
+import { StyleSheet } from 'react-native';
 
 export const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#676767',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  Logo: {
+    alignItems: 'center',
+    width: 300,
+    height: 100,
+    backgroundColor: '#000000',
+    overflow: 'hidden',
+  }
 });
